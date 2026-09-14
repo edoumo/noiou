ee992f0024787841e5091fbc3f4e2541394cc607
edoumo <100148844+edoumo@users.noreply.github.com>
2026-09-14 21:32:47 +0200

    Expose session reset control in app shell

--- src/AppShell.tsx
+++ src/AppShell.tsx
@@ -2,11 +2,14 @@ import App from './App';
 import InteractionPreferences from './InteractionPreferences';
 import NwcReceiveDiagnostic from './NwcReceiveDiagnostic';
 import { NwcSessionProvider } from './NwcSessionContext';
+import SessionResetControl from './SessionResetControl';
+import './uxFixes.css';
 
 export default function AppShell() {
   return (
     <NwcSessionProvider>
       <App />
+      <SessionResetControl />
       <NwcReceiveDiagnostic />
       <InteractionPreferences />
     </NwcSessionProvider>
